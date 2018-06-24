c8e2ee69aba15ed662b043ccbc73eab57a4e28be
EC2 Default User <ec2-user@ip-172-31-16-40.us-west-2.compute.internal>
2018-06-24 05:35:35 +0000

    Cleaned up a few bugs and added distance module

--- src/load.py
+++ src/load.py
@@ -3,27 +3,22 @@
 # 6/23/18
 
 from game import Game
-from location import LocationMatrix
 from moment import Moment
 from player import Player
 from team import Team
-from utils import get_logger
 
 from datetime import datetime
 
 import json
 
-logger = get_logger('load')
-
-
-def load_game(game_path='../games/0021500503.json'):
+def load_game(game_path, logger):
     '''
     '''
     with open(game_path, 'r') as g:
         logger.info("Reading the raw SportVU data into memory from {}...".format(game_path))
         d = json.loads(g.read())
 
-        logger.info("Reading in the player data...")
+        logger.info("Reading in the player and team data...")
         visitor_players = [
             Player(
                 x['firstname'], 
@@ -54,11 +49,10 @@ def load_game(game_path='../games/0021500503.json'):
             d['events'][0]['home']['abbreviation'],
             home_players
         )
-        game = Game(d['gamedate'], d['gameid'], home_team, visitor_team)
-        moments = [] 
-        for moment_array in d['events'][1]['moments'][:4]:
+        
+        logger.info("Reading in the location data...")
+        moments = []
+        for moment_array in d['events'][0]['moments']:
             moments.append(Moment(moment_array))
-        print(moments)
-         
+        return Game(d['gamedate'], d['gameid'], home_team, visitor_team, moments)         
 
-load_game()
