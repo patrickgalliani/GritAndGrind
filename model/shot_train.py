93bd2fcdee42865f0a4c6b0a5a4553e86b001690
Ricky Galliani <pjgalliani@gmail.com>
2018-06-26 06:58:30 +0000

    Linear regression and decision tree regressor model

--- model/shot_train.py
+++ model/shot_train.py
@@ -4,14 +4,16 @@
 
 from utils import get_logger
 
-from sklearn import svm
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.externals import joblib
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import DecisionTreeRegressor
 
 import numpy as np
 import pandas as pd
+import pickle
 
 BUNNY = 0
 MIDRANGE = 1
@@ -86,23 +88,31 @@ if __name__ == '__main__':
         test_size=0.33,
         random_state=22
     )
-    logger.info("X_train:\n{}\n({})\n".format(X_train[:3], len(X_train)))
-    logger.info("X_test:\n{}\n({})\n".format(X_test[:3], len(X_test)))
-    logger.info("y_train:\n{}\n({})\n".format(y_train[:3], len(y_train)))
-    logger.info("y_test:\n{}\n({})\n".format(y_test[:50], len(y_test)))
+    logger.info("\nX_train:\n{}\n({})\n".format(X_train[:3], len(X_train)))
+    logger.info("\nX_test:\n{}\n({})\n".format(X_test[:3], len(X_test)))
+    logger.info("\ny_train:\n{}\n({})\n".format(y_train[:3], len(y_train)))
+    logger.info("\ny_test:\n{}\n({})\n".format(y_test[:3], len(y_test)))
 
     # Fit decision tree regressor model
-    dt = DecisionTreeRegressor(max_depth=3)
-    dt.fit(X_train, y_train.ravel())
+    dt = DecisionTreeRegressor(max_depth=4)
+    dt.fit(X_train, y_train)
+    dt_params = dt.get_params(deep=True)
+    logger.info("\nDT Regressor Model Parameters\n\{}\n".format(dt_params))
+    joblib.dump(dt, 'dt.pkl')
+    logger.info("Persisted the DT Regressor model to dt.pkl...")
     y_pred_dt = dt.predict(X_test)
 
-    # Fit svm model
-    svm = svm.SVC()
-    svm.fit(X_train, y_train.ravel())
-    y_pred_svm = svm.predict(X_test) 
+    # Fit Linear Regression model
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    lr_params = lr.coef_
+    logger.info("\nLinear Regressor Model Parameters\n{}\n".format(lr_params))
+    joblib.dump(lr, 'lr.pkl')
+    logger.info("Persisted the Linear Regression model to lr.pkl...")
+    y_pred_lr = lr.predict(X_test)
 
     # Compute mse of model
     mse_dt = mean_squared_error(y_test, y_pred_dt)
-    mse_svm = mean_squared_error(y_test, y_pred_svm)
+    mse_lr = mean_squared_error(y_test, y_pred_lr)
     logger.info("Mean Squared Error (DT): {}".format(mse_dt))
-    logger.info("Mean Squared Error (SVM): {}".format(mse_svm)) 
+    logger.info("Mean Squared Error (LR): {}".format(mse_lr))
